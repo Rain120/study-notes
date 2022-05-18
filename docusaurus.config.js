0d1a894bdb1f8b8ee115ac9fe819d8c5b2e0c662
Rain120 <1085131904@qq.com>
2022-05-18 20:38:50 +0800

    feat(config): update announcementBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
       announcementBar: {
         id: "feature_release", // Any value that will identify this message.
         content: `
-          <a href='https://rain120.github.io/study-notes/books/read/react/a-hundred-things-i-learned-working-on-the-react-team'>阅书阁</a> 
+          <a href='https://rain120.github.io/study-notes/books/read/engineering/houjin-exploration-and-practice-of-micro-front-end-technology'>阅书阁 侯晋 - 抖音电商微前端技术探索与实践</a> 
           新上线，欢迎体验
         `,
         // 文章📚 
